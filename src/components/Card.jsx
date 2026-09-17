0d6d554ffe1eea874f97d2c6e3ba716b9d31427f
wishyrater <julian.cesar.andersen@gmail.com>
2026-09-17 02:01:50 +0200

    Fix card styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import "./Card.css";
 export default function Card({ onClick, champion }) {
     return (
         <div className="card" onClick={() => onClick(champion)} >
-            <img src={champion.imageUrl} alt="Splash art" style={{maxWidth: "100%", height:"auto"}}/>
+            <img src={champion.imageUrl} alt="Splash art"/>
             <span><p>{champion.name}</p></span>
         </div>
     )
